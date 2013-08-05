20d4967125c2b1f6bb343e6606a83ded1919cc7d
Liu Yang <zation1@gmail.com>
2013-08-06 09:36:20 +1000

    init project

---
+++ demo/main.js
@@ -0,0 +1,17 @@
+$(function() {
+  $.ajax({
+    url: 'http://localhost:3000/font',
+    dataType: 'jsonp',
+    data: {
+      'charactors': '昨日'
+    },
+    success: function(data) {
+      var fontface = '@font-face {' +
+        'font-family: test;' +
+        'src: url(http://localhost:3000' + data.fontPath + ');' +
+        'font-weight:400;' +
+        '}';
+      $('head').append('<style>' + fontface + '</style>');
+    }
+  });
+});
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var spawn = require('child_process').spawn;
+
+var app = express();
+
+app.get('/font', function(req, res) {
+  var charactors = req.param('charactors');
+  console.log(charactors);
+  var output = '/fonts/output.ttf';
+  var input = '../font-lib/test.ttf'
+  var subset = spawn('perl', [
+    'subset.pl',
+    '--chars="' + charactors + '"',
+    input,
+    '../' + output
+  ], {
+    encoding: 'utf8',
+    cwd: './perl'
+  });
+
+  subset.stdout.on('data', function(data) {
+    console.log('stdout: \n' + data);
+  });
+
+  subset.stderr.on('data', function(data) {
+    console.log('stderr: \n' + data);
+  });
+
+  subset.on('close', function() {
+    res.jsonp({
+      'fontPath': output
+    });
+  });
+});
+
+app.use('/fonts', express.static('./fonts'));
+
+app.listen(3000);
